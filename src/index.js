59c8c0a9f793203df765758eeba8b481fb956ecc
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-15 17:28:15 +0100

    Extract Component component

--- src/index.js
+++ src/index.js
@@ -1,28 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from "faker";
+
+import Comment from "./Comment";
 
 class App extends React.Component {
   render() {
     return (
       <div className="ui container comments">
-        <div className="comment">
-          <a href="/" className="avatar">
-            <img alt="avatar" src={faker.image.avatar()} />
-          </a>
-
-          <div className="content">
-            <a href="/" className="author">
-              Sam
-            </a>
-
-            <div className="metadata">
-              <span className="date">Today at 6:00PM</span>
-            </div>
-
-            <div className="text">Nice blog post!</div>
-          </div>
-        </div>
+        <Comment />
+        <Comment />
+        <Comment />
       </div>
     );
   }
