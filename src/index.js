ceca1962b7d2bf7af5c37179b4bf490ff1dbd246
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-15 17:12:16 +0100

    Add static content for component with Semantic UI styling

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   render() {
     return (
+      <div className="ui container comments">
+        <div className="comment">
+          <a href="/" className="avatar">
+            <img alt="avatar" />
+          </a>
+
+          <div className="content">
+            <a href="/" className="author">
+              Sam
+            </a>
+
+            <div className="metadata">
+              <span className="date">Today at 6:00PM</span>
+            </div>
+
+            <div className="text">Nice blog post!</div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
