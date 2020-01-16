3426bf2862eaa5e358f098b3fab0bbfafcd7250d
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-16 09:38:30 +0100

    Allow to use nested components

--- src/index.js
+++ src/index.js
@@ -2,30 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 
+import ApprovalCard from "./ApprovalCard";
 import Comment from "./Comment";
 
 class App extends React.Component {
   render() {
     return (
       <div className="ui container comments">
-        <Comment
-          author="Sam"
-          timeAgo="Today at 6:00PM"
-          content="You are crazy, man!"
-          avatar={faker.image.avatar()}
-        />
-        <Comment
-          author="Alex"
-          timeAgo="Today at 2:00AM"
-          content="Nice blog post!"
-          avatar={faker.image.avatar()}
-        />
-        <Comment
-          author="Jane"
-          timeAgo="Yesterday at 5:00PM"
-          content="Coolf!"
-          avatar={faker.image.avatar()}
-        />
+        <ApprovalCard>
+          <Comment
+            author="Sam"
+            timeAgo="Today at 6:00PM"
+            content="You are crazy, man!"
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+
+        <ApprovalCard>
+          <Comment
+            author="Alex"
+            timeAgo="Today at 2:00AM"
+            content="Nice blog post!"
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+
+        <ApprovalCard>
+          <Comment
+            author="Jane"
+            timeAgo="Yesterday at 5:00PM"
+            content="Cool!"
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
       </div>
     );
   }
